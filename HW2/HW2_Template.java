1fbd7aa00718bb33bbb0b8fdd706740f53af42cc
fygure <92011471+fygure@users.noreply.github.com>
2024-03-06 14:40:05 -0600

    updated template

--- HW2/HW2_Template.java
+++ HW2/HW2_Template.java
@@ -13,20 +13,20 @@ import java.util.Scanner;
 // BEGIN SOLUTION
 // Please import only standard libraries and make sure that your code compiles and runs without unhandled exceptions 
 // END SOLUTION
-
-public class HW2 {
+ 
+public class HW2 {    
 
   static void P1() throws Exception {
     byte[] cipherBMP = Files.readAllBytes(Paths.get("cipher1.bmp"));
-
+    
     // BEGIN SOLUTION
-    byte[] key = new byte[] { 0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0 };
-    byte[] plainBMP = cipherBMP;
+    byte[] key = new byte[] { 0, 0, 0, 0, 
+                              0, 0, 0, 0, 
+                              0, 0, 0, 0, 
+                              0, 0, 0, 0 };
+    byte[] plainBMP = cipherBMP;    
     // END SOLUTION
-
+    
     Files.write(Paths.get("plain1.bmp"), plainBMP);
   }
 
@@ -39,19 +39,19 @@ public class HW2 {
     byte[] plain = modifiedCipher;
 
     // END SOLUTION
-
+    
     Files.write(Paths.get("plain2.txt"), plain);
   }
 
   static void P3() throws Exception {
     byte[] cipherBMP = Files.readAllBytes(Paths.get("cipher3.bmp"));
     byte[] otherBMP = Files.readAllBytes(Paths.get("plain1.bmp"));
-
+    
     // BEGIN SOLUTION
     byte[] modifiedBMP = cipherBMP;
 
     // END SOLUTION
-
+    
     Files.write(Paths.get("cipher3_modified.bmp"), modifiedBMP);
   }
 
@@ -59,38 +59,38 @@ public class HW2 {
     byte[] plainA = Files.readAllBytes(Paths.get("plain4A.txt"));
     byte[] cipherA = Files.readAllBytes(Paths.get("cipher4A.txt"));
     byte[] cipherB = Files.readAllBytes(Paths.get("cipher4B.txt"));
-
+    
     // BEGIN SOLUTION
     byte[] plainB = cipherB;
 
     // END SOLUTION
-
+    
     Files.write(Paths.get("plain4B.txt"), plainB);
   }
 
   static void P5() throws Exception {
     byte[] cipherBMP = Files.readAllBytes(Paths.get("cipher5.bmp"));
-
+    
     // BEGIN SOLUTION
     byte[] plainBMP;
-    byte[] key = new byte[] { 0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0 }; // byte array size
+    byte[] key = new byte[] {   0,   0,    0,   0, 
+                                0,   0,    0,   0,
+                                0,   0,    0,   0,
+                                0,   0,    0,   0 }; // byte array size
     // try {
-    plainBMP = cipherBMP;
-    // decryption might throw a BadPaddingException!
+      plainBMP = cipherBMP;
+      // decryption might throw a BadPaddingException!
     // }
     // catch (BadPaddingException e) {
     // }
 
     // END SOLUTION
-
+    
     Files.write(Paths.get("plain5.bmp"), plainBMP);
   }
 
-  public static void main(String[] args) {
-    try {
+  public static void main(String [] args) {
+    try {  
       P1();
       P2();
       P3();
@@ -98,6 +98,6 @@ public class HW2 {
       P5();
     } catch (Exception e) {
       e.printStackTrace();
-    }
+    } 
   }
 }
