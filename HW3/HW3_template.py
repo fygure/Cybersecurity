de610be132ef3e7438a00d057276793b55cf2604
fygure <92011471+fygure@users.noreply.github.com>
2024-04-11 18:27:02 -0500

    starter files for HW3

---
+++ HW3/HW3_template.py
@@ -0,0 +1,35 @@
+# This is the template for Problem 1 only.
+# For problems 2 and 3, keep the class definitions
+# the same and rewrite the main program
+import rsa
+key_size = 2048
+
+class Sender:
+    # BEGIN SOLUTION
+    #
+    #
+    # END SOLUTION
+
+class Receiver:
+    # BEGIN SOLUTION
+    #
+    #
+    # END SOLUTION
+
+if __name__ == "__main__":
+    with open('message1.txt', 'rb') as file:
+        plaintext = file.read()
+    # initialize sender and receiver objects with 2048 key_size
+
+    # Sender Side
+    # BEGIN SOLUTION
+    #
+    #
+    # END SOLUTION
+
+    # Receiver Side
+    # BEGIN SOLUTION
+    #
+    #
+    # END SOLUTION
+
